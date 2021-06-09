361d314ed1452f4e806bef07a2c49a294345d653
kar1mov <kar1mov.nur@gmail.com>
2021-06-09 18:47:08 +0600

    initial

---
+++ javascript/script.js
@@ -0,0 +1,17 @@
+$('.project__slider').slick({
+    arrows: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    dots: false,
+    autoPlay: true,
+    autoplaySpeed: 2000,
+});
+
+$('.arenda__slider').slick({
+    arrows: true,
+    slidesToShow: 1,
+
+    dots: false,
+    autoPlay: true,
+    autoplaySpeed: 2000,
+});
